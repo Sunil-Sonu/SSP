991ea6627633caadc7ef8f0d0b58edeeef65621b
= <=>
2017-04-05 08:41:33 +0000

    modified mysql db password in settings.py file

--- Test/settings.py
+++ Test/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'myproject',
         'USER': 'root',
-        'PASSWORD': 'root',
+        'PASSWORD': 'toor',
         'HOST': 'localhost',
         'PORT': '3306',
     }
